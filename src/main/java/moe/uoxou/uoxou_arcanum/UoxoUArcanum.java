3007497357386a42376118d6aead534b3cc55a7c
Zemelua <zemelua1107@gmail.com>
2025-08-29 05:01:11 +0900

    〖 chore 〗Change the mappings to Yarn

--- src/main/java/moe/uoxou/uoxou_arcanum/UoxoUArcanum.java
+++ src/main/java/moe/uoxou/uoxou_arcanum/UoxoUArcanum.java
@@ -2,7 +2,7 @@ package moe.uoxou.uoxou_arcanum;
 
 import moe.uoxou.uoxou_arcanum.block.ModBlocks;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Identifier;
 
 public class UoxoUArcanum implements ModInitializer {
 	public static final String MOD_ID = "uoxou_arcanum";
@@ -12,7 +12,7 @@ public class UoxoUArcanum implements ModInitializer {
 		ModBlocks.init();
 	}
 
-	public static ResourceLocation identifier(String path) {
-		return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
+	public static Identifier identifier(String path) {
+		return Identifier.of(MOD_ID, path);
 	}
 }
--- src/main/java/moe/uoxou/uoxou_arcanum/block/ModBlocks.java
+++ src/main/java/moe/uoxou/uoxou_arcanum/block/ModBlocks.java
@@ -1,22 +1,22 @@
 package moe.uoxou.uoxou_arcanum.block;
 
 import moe.uoxou.uoxou_arcanum.UoxoUArcanum;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 
 public final class ModBlocks {
 	public static final Block MANA_CAULDRON = new ManaCauldronBlock();
 
-	public static final ResourceKey<Block> KEY_MANA_CAULDRON = ResourceKey.create(Registries.BLOCK, UoxoUArcanum.identifier("mana_cauldron"));
+	public static final RegistryKey<Block> KEY_MANA_CAULDRON = RegistryKey.of(RegistryKeys.BLOCK, UoxoUArcanum.identifier("mana_cauldron"));
 
 	public static void init() {
-		Registry.register(BuiltInRegistries.BLOCK, KEY_MANA_CAULDRON, MANA_CAULDRON);
+		Registry.register(Registries.BLOCK, KEY_MANA_CAULDRON, MANA_CAULDRON);
 
-		Item.BY_BLOCK.put(MANA_CAULDRON, Items.CAULDRON);
+		Item.BLOCK_ITEMS.put(MANA_CAULDRON, Items.CAULDRON);
 	}
 }
